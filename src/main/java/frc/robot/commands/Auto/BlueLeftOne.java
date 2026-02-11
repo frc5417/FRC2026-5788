ada75eec4def92d76fa7d811a10c8eb87426d636
curiousprogrammer1 <mzhjiang21@gmail.com>
2026-02-10 18:24:40 -0600

    Create BlueLeftOne.java

---
+++ src/main/java/frc/robot/commands/Auto/BlueLeftOne.java
@@ -0,0 +1,14 @@
+
+package frc.robot.commands.Autos;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.CustomNamedCommands;
+import frc.robot.commands.AutoControllers.FollowBezier;
+import frc.robot.subsystems.DriveBase;
+
+public class BlueLeftOne extends SequentialCommandGroup {
+    
+}
