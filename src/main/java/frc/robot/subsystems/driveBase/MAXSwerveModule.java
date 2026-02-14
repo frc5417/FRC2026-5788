73eb398fc0458076d4e4a37d465d8e3b386d996e
Sai Kondoji <samridh.kondoji@gmail.com>
2026-02-14 13:11:17 -0600

    I have added MAXSwerve module subsystem from REV robotics

--- src/main/java/frc/robot/subsystems/driveBase/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/driveBase/MAXSwerveModule.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.driveBase;
+package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -51,9 +51,9 @@ public class MAXSwerveModule {
     // Apply the respective configurations to the SPARKS. Reset parameters before
     // applying the configuration to bring the SPARK to a known good state. Persist
     // the settings to the SPARK to avoid losing them on a power cycle.
-    m_drivingSpark.configure(frc.robot.subsystems.driveBase.drivingConfig, ResetMode.kResetSafeParameters,
+    m_drivingSpark.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
-    m_turningSpark.configure(frc.robot.subsystems.driveBase.turningConfig, ResetMode.kResetSafeParameters,
+    m_turningSpark.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
 
     m_chassisAngularOffset = chassisAngularOffset;
