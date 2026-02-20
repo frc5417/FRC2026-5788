a938dafcf81266cc2d48ea270aa3ebeec5f4d123
VirkMehpreet <virkmehpreet@gmail.com>
2026-02-19 18:22:36 -0600

    Synced to EveryBot code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,59 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity. 
+ */
 public final class Constants {
 
-  // ---------------- Controller ---------------- 
-  public static final class ControllerConstants {
+  public static final class DriveConstants {
+    // Motor controller IDs for drivetrain motors
+    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_FOLLOWER_ID = 3;
+    public static final int RIGHT_LEADER_ID = 2;
+    public static final int RIGHT_FOLLOWER_ID = 4;
+
+    // Current limit for drivetrain motors. 60A is a reasonable maximum to reduce
+    // likelihood of tripping breakers or damaging CIM motors
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+  }
+
+  public static final class FuelConstants {
+    // Motor controller IDs for Fuel Mechanism motors
+    public static final int LEFT_INTAKE_LAUNCHER_MOTOR_ID = 5;
+    public static final int RIGHT_INTAKE_LAUNCHER_MOTOR_ID = 6;
+    public static final int INDEXER_MOTOR_ID = 8;
+
+    // Current limit for fuel mechanism motors.
+    public static final int INDEXER_MOTOR_CURRENT_LIMIT = 80;
+    public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 80;
+
+    // All values likely need to be tuned based on your robot
+    public static final double INDEXER_INTAKING_PERCENT = -.8; 
+    public static final double INDEXER_LAUNCHING_PERCENT = 0.6;
+    public static final double INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT = -0.5;
+
+    public static final double INTAKE_INTAKING_PERCENT = 0.6;
+    public static final double LAUNCHING_LAUNCHER_PERCENT = .85;
+    public static final double INTAKE_EJECT_PERCENT = -0.8;
+
+    public static final double SPIN_UP_SECONDS = 0.75;
+  }
+
+  public static final class ClimbConstatns {
+    // Motor controller IDs for Climb motor
+    public static final int CLIMBER_MOTOR_ID = 7;
+
+    // Current limit for climb motor
+    public static final int CLIMBER_MOTOR_CURRENT_LIMIT = 40;
+    // Percentage to power the motor both up and down
+    public static final double CLIMBER_MOTOR_DOWN_PERCENT = -0.8;
+    public static final double CLIMBER_MOTOR_UP_PERCENT = 0.8;
+  }
+
+  public static final class OperatorConstants {
+
+    // Port constants for driver and operator controllers. These should match the
+    // values in the Joystick tab of the Driver Station software
     public static final int DRIVER_CONTROLLER_PORT = 0;
-     public static final int kDriverControllerPort = 00;      // USB port for driver controller
-        public static final int kManipulatorControllerPort = 0; // USB port for manipulator controller
-        public static final double joystickDeadband = 0.05;     // Deadzone for joysticks
-  }
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
 
-
-  
-  // ---------------- Drivebase ----------------
-  public static final class DriveBaseConstants {
-    // Set once we know our gyro
-    // public static final int GYRO_ID = ;
-
-  }
-
-  // swerev modules
-  public static final class ModuleConstants {
-    // Motor / encoder IDs (fill in when hardware is known)
-     public static final int[] DRIVE_MOTOR_IDS = {11,41,21,31};
-     public static final int[] ANGLE_MOTOR_IDS = {12, 42, 22, 32};
-     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-     public static final double kFrontRightChassisAngularOffset = 0;
-     public static final double kBackLeftChassisAngularOffset = Math.PI;
-     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
-   // public static final int[] ENCODER_IDS = {};
-     public static final int kDrivingMotorPinionTeeth = 14;
-
-    // Calculations required for driving motor conversion factors and feed forward
-    public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
-    public static final double kWheelDiameterMeters = 0.0762;
-    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
-    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
-    // teeth on the bevel pinion
-    public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
-    public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
-        / kDrivingMotorReduction;
-    // Offsets (degrees)
-    // public static final double[] ANGLE_OFFSETS = {};
-  }
-
-  //---------------- Robot Geometry ----------------
-  public static final class RobotConstants {
-    // Distance from robot center to each module
-    public static final double MODULE_TO_CENTER =0 ;
-
-    // Wheel size
-    // public static final double WHEEL_RADIUS = ;
-  }
-
-  public static final class NeoMotorConstants {
-    public static final double kFreeSpeedRpm = 5676;
-  }
+    // This value is multiplied by the joystick value when rotating the robot to
+    // help avoid turning too fast and beign difficult to control
+    public static final double DRIVE_SCALING = 0.7;
+    public static final double ROTATION_SCALING = 0.8;
+  } 
 }
