991aca2ceccec06eb87bc1428b86cd3d329847fc
VirkMehpreet <virkmehpreet@gmail.com>
2026-02-27 17:17:15 -0600

    Adding CanIDs for all the 4 MaxSwerve motors

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,10 +6,10 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public class SwerveSubsystem extends SubsystemBase {
 
-    private final MaxSwerveModule frontLeft = new MaxSwerveModule(1, 2);
-    private final MaxSwerveModule frontRight = new MaxSwerveModule(3, 4);
-    private final MaxSwerveModule backLeft = new MaxSwerveModule(5, 6);
-    private final MaxSwerveModule backRight = new MaxSwerveModule(7, 8);
+    private final MaxSwerveModule frontLeft = new MaxSwerveModule(31, 32);
+    private final MaxSwerveModule frontRight = new MaxSwerveModule(21, 22);
+    private final MaxSwerveModule backLeft = new MaxSwerveModule(41, 42);
+    private final MaxSwerveModule backRight = new MaxSwerveModule(11, 12);
 
     private final SwerveDriveKinematics kinematics =
         new SwerveDriveKinematics(
