af9a6f473d6a1f70d1503e7dcb2947a3004f2849
kiransnambiar001 <kiransnambiar001@gmail.com>
2026-03-13 01:37:26 -0500

    Implemented PathPlanner & Fixed FieldCentric Turning

---
+++ src/main/java/frc/robot/commands/ShootAuto.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class ShootAuto extends Command {
+    private final ShooterSubsystem shooterSubsystem;
+
+    public ShootAuto(ShooterSubsystem shooterSubsystem) {
+        this.shooterSubsystem = shooterSubsystem;
+
+        addRequirements(shooterSubsystem);
+        this.finallyDo(interrupted -> shooterSubsystem.stopAll());
+    }
+
+    @Override
+    public void execute() {
+        shooterSubsystem.runFlywheel(-3000);
+
+        if (shooterSubsystem.isReady()) {
+            shooterSubsystem.runFeeder(-0.6);
+        }
+        else {shooterSubsystem.runFeeder(0);}
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.stopAll();
+    }
+}
